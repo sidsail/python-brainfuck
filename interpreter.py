d846ad8a6189c7157c41a6366e775f5cf468c2b9
sidsail <sidsailcloud@gmail.com>
2022-10-25 18:40:01 -0700

    commit

--- interpreter.py
+++ interpreter.py
@@ -8,12 +8,11 @@ def interpret(code: str):
 		if v == "]":
 			try:
 				a = arr.pop()
+				m[a] = i
 			except IndexError:
 				raise Exception("braces mismatch(more closing than opening)")
-			m[a] = i
 	if len(arr) != 0:
 		raise Exception("braces mismatch(more opening then closing)")
-
 	tape = [0]
 	p = 0
 	cp = 0
@@ -56,6 +55,5 @@ def interpret(code: str):
 				keys = [k for k, v in m.items() if v == cp]
 				cp = keys[0]
 				continue
-
 		cp += 1
 	return output

4397c38840f2fbe12d4f78d5506db566804bb96c
sidsail <sidsailcloud@gmail.com>
2022-10-25 17:45:08 -0700

    init

---
+++ interpreter.py
@@ -0,0 +1,61 @@
+def interpret(code: str):
+	code = code.replace(" ", "")
+	arr = []
+	m = {}
+	for i, v in enumerate(code):
+		if v == "[":
+			arr.append(i)
+		if v == "]":
+			try:
+				a = arr.pop()
+			except IndexError:
+				raise Exception("braces mismatch(more closing than opening)")
+			m[a] = i
+	if len(arr) != 0:
+		raise Exception("braces mismatch(more opening then closing)")
+
+	tape = [0]
+	p = 0
+	cp = 0
+	l = len(tape)
+	lc = len(code)
+	output = ""
+	while cp < lc:
+		if code[cp] == "":
+			cp += 1
+			continue
+		elif code[cp] == " ":
+			cp += 1
+			continue
+		elif code[cp] == "+":
+			tape[p] += 1
+		elif code[cp] == "-":
+			if tape[p] == 0:
+				tape[p] = 255
+			tape[p] -= 1
+		elif code[cp] == ">":
+			if p == l-1:
+				tape.append(0)
+				l += 1
+			p += 1
+		elif code[cp] == "<":
+			if p == 0:
+				raise Exception("trying to go before tape start")
+			p -= 1
+		elif code[cp] == "[":
+			if tape[p] == 0:
+				cp = m[cp]
+				continue
+		elif code[cp] == ".":
+			output += chr(tape[p])
+		elif code[cp] == ",":
+			i = input("> ")
+			tape[p] = ord(i)
+		elif code[cp] == "]":
+			if tape[p] != 0:
+				keys = [k for k, v in m.items() if v == cp]
+				cp = keys[0]
+				continue
+
+		cp += 1
+	return output

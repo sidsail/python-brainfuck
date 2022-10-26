005a219c94c1fffa82afd77fc998de69b9c421fe
sidsail <sidsailcloud@gmail.com>
2022-10-26 13:04:34 -0700

    improved

--- interpreter.py
+++ interpreter.py
@@ -1,28 +1,12 @@
 def interpret(code: str):
-	code = code.replace(" ", "")
-	arr = []
-	m = {}
-	for i, v in enumerate(code):
-		if v == "[":
-			arr.append(i)
-		if v == "]":
-			try:
-				a = arr.pop()
-				m[a] = i
-			except IndexError:
-				raise Exception("braces mismatch(more closing than opening)")
-	if len(arr) != 0:
-		raise Exception("braces mismatch(more opening then closing)")
 	tape = [0]
 	p = 0
 	cp = 0
 	l = len(tape)
 	lc = len(code)
 	output = ""
+	stack = []
 	while cp < lc:
-		if code[cp] == "":
-			cp += 1
-			continue
 		if code[cp] == " ":
 			cp += 1
 			continue
@@ -41,19 +25,32 @@ def interpret(code: str):
 			if p == 0:
 				raise Exception("trying to go before tape start")
 			p -= 1
+		
 		if code[cp] == "[":
 			if tape[p] == 0:
-				cp = m[cp]
+				c = 1
+				for i, v in enumerate(code[:cp]):
+					if v == "[":
+						c += 1
+					if v == "]":
+						c -= 1
+					if c == 0:
+						cp = i
+						continue
+				else:
+					raise Exception("mismatched braces")
+			else:
+				stack.append(cp)
+		if code[cp] == "]":
+			if tape[p] != 0:
+				cp = stack.pop()
 				continue
+		
 		if code[cp] == ".":
 			output += chr(tape[p])
 		if code[cp] == ",":
 			i = input("> ")
 			tape[p] = ord(i)
-		if code[cp] == "]":
-			if tape[p] != 0:
-				keys = [k for k, v in m.items() if v == cp]
-				cp = keys[0]
-				continue
-		cp += 1
+		cp += 1	
+	print(tape)
 	return output
---
+++ test.py
@@ -0,0 +1,13 @@
+import interpreter
+
+def load_file():
+	with open("helloworld.bf" , "r") as file:
+		return str(file.read())
+
+code = load_file()
+
+def test(code):
+	output = interpreter.interpret(code)
+	print(output)
+
+test(code)
